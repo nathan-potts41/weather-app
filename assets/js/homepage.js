c076b6685218742883cc4623a3f469710879be5d
nathan-potts41 <pottsandpans41@hotmail.com>
2020-11-30 17:25:44 -0600

    updated functions

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,21 +1,36 @@
-//function fetching the data from the api
-var getCityData = function () {
+var weatherUrl = "https://api.openweathermap.org/data/2.5/forecast?q="
+var apiKey = "&appid=a41a732753ea4b9aa10d848013b989de"
+// function fetching the city from the api
+var getCity = function (event) {
     event.preventDefault();
-    var cityEntry = document.getElementById("city-search").value;
-    var searchStr = fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + cityEntry + "&appid=a41a732753ea4b9aa10d848013b989de")
+    var cityEntry = $("#city-search");
+    var city = $.trim(cityEntry.val());
+    console.log(city);
+
+    getCityData(city);
+};
+
+// function fetching the json data for above city
+var getCityData = function (city) {
+    var searchStr = fetch(weatherUrl + city + apiKey)
         .then(function (response) {
             response.json().then(function (data) {
                 console.log(data)
             });
         });
-};
+}
 
+// function for current day forecast
+var displayForecast = function () {
+    
+}
 
-// var displayData = function () {
-
+// function for 5 day forcast
+// var display5Day = function () {
+//     
 // }
 
-// document.addEventListener("load", )
-document.getElementById("submit").addEventListener("click", getCityData);
+// $('#city-search').ready(getCityData);
+document.getElementById("submit").addEventListener("click", getCity);
 
 
